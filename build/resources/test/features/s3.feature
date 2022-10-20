9e0ae4c4102fe47e27b6d8c1632b0d01277ab013
lpetro <lapetro@bancolombia.com.co>
2022-10-20 17:07:22 -0500

    Solucion final

--- build/resources/test/features/s3.feature
+++ build/resources/test/features/s3.feature
@@ -1,32 +1,12 @@
 Feature: get the log
   Background:
-    * def urlbase = "https://s3.console.aws.amazon.com/s3/buckets?region=us-east-1"
-    * def fileName = "year=2022"
-    * def bucket = "nu0286001-central-channel-repository-qa-daily-raw-date"
+    * def bucket = "nu0286001-central-channel-repository-qa-daily-raw-data"
     * def aws = Java.type('AWS.AwsConnect')
 
-  @IfExist
-  Scenario: I want to list the files
-    * configure ssl = true
-    Given def bol = aws.checkIfFileExists(bucket, fileName)
-    When print bol
-    Then match bol == true
-
-  @ListS3
-  Scenario: I want to list all files
-    Given def bol = aws.ShowContent(bucket)
-    When print bol
-    Then match bol contains fileName
-
-  @Validate
-  Scenario Outline: Validate field in file S3
-    Given json resJson =  aws.readFileS3(bucket,fileName)
-    When  print resJson
-    Then  match resJson.documentType   == "<documenttype>"
-    And   match resJson.documentNumber == "<documentnumber>"
-
-    Examples:
-      |   documenttype   | documentnumber   |
-      |    CC            |   202203282    |
 
 
+  @ValidateLastRegister
+  Scenario: Validate last register
+    Given def result = aws.readFileS3(bucket)
+    When print result
+    Then match result.transactionId contains "d26304c5-f2e3-71a1-3c75-0279b432aff1"
--- src/test/java/AWS/AwsConnect.java
+++ src/test/java/AWS/AwsConnect.java
@@ -7,50 +7,19 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class AwsConnect {
+
     private static AWSCredentials getSessionCredentials() {
-        /*return new BasicSessionCredentials(
+        return new BasicSessionCredentials(
                 System.getenv("accessKey"),
                 System.getenv("secretKey"),
                 System.getenv("sessionToken")
-        );*/
-        return new BasicAWSCredentials(
-                System.getenv("clientid"),
-                System.getenv("secretid")
         );
     }
 
-    public static boolean checkIfFileExists(String bucket, String fileName) {
-        boolean existsJson = false;
-        try {
-            AmazonS3 s3 = AmazonS3ClientBuilder
-                    .standard()
-                    .withRegion(Regions.US_EAST_1)
-                    .withCredentials(new AWSStaticCredentialsProvider(getSessionCredentials()))
-                    .build();
-            existsJson = doesItExists(s3, bucket, fileName);
-        } catch (Exception e) {
-            throw e;
-        }
-        return existsJson;
-    }
-
-    public static boolean doesItExists(AmazonS3 s3, String bucket, String fileName) {
-        boolean doesItExists = false;
-        try {
-            doesItExists = s3.doesObjectExist(bucket, fileName); //s3.doesBucketExistV2(bucket);
-        } catch (Exception error) {
-            boolean isUploadedFile = false;
-            throw new IllegalStateException(error);
-        }
-        return doesItExists;
-    }
-
-
-    public static String readFileS3(String bucket, String fileName) throws IOException {
+    public static String readFileS3(String bucket) throws IOException {
 
         try {
             AmazonS3 s3 = AmazonS3ClientBuilder
@@ -58,6 +27,7 @@ public class AwsConnect {
                     .withRegion(Regions.US_EAST_1)
                     .withCredentials(new AWSStaticCredentialsProvider(getSessionCredentials()))
                     .build();
+            String fileName = lastRegister(bucket);
             S3Object o = s3.getObject(bucket, fileName);
             S3ObjectInputStream s3is = o.getObjectContent();
             StringBuilder sb = new StringBuilder();
@@ -76,9 +46,9 @@ public class AwsConnect {
     }
 
 
-    public static ArrayList<String> filesInS3(String bucket) {
-        String list = "";
-        ArrayList<String> Content = new ArrayList<String>();
+    public static ArrayList<String> listFilesInS3(String bucket) {
+
+        ArrayList<String> Content = new ArrayList();
         try {
             AmazonS3 s3 = AmazonS3ClientBuilder
                     .standard()
@@ -87,11 +57,9 @@ public class AwsConnect {
                     .build();
             ListObjectsV2Result result = s3.listObjectsV2(bucket);
             List<S3ObjectSummary> objects = result.getObjectSummaries();
-            //list = objects.toString();
             for (S3ObjectSummary os : objects) {
-                list = list + ", " + os.getKey();
                 Content.add(os.getKey());
-                System.out.println("********" + os.getKey());
+                System.out.println("*******>>" + os.getKey());
             }
             return Content;
         } catch (Exception e) {
@@ -99,76 +67,15 @@ public class AwsConnect {
         }
     }
 
-    public static String ShowContent(String bucket) {
-        String list = "";
-        ArrayList<String> Content = new ArrayList<String>();
-        ArrayList<Integer> Content2 = new ArrayList<Integer>();
-       Content = filesInS3(bucket);
 
-        for (int i=0;i<Content.size();i++) {
-            list = list + ", " + Content.get(i);
-            System.out.println("--->"+Content.get(i));
-        }
-
-        for(int j=0;j< Content.size();j++){
-            if(ClasificationYears(Content.get(j)) != 0) {
-                Content2.add(ClasificationYears(Content.get(j)));
-                System.out.println("_-_-" + ClasificationYears(Content.get(j)));
-            }
-        }
-      //Integer UltYear = Integer.parseInt(Collections.max(Content2).toString());
-     return list;
+    public static String lastRegister(String bucket) {
+        String lastRegitesInS3 = "";
+        ArrayList<String> Content = listFilesInS3(bucket);
+        int lastElement = Content.size() - 1;
+        lastRegitesInS3 = Content.get(lastElement).toString();
+        System.out.println("=======lastRegister====>>" + lastRegitesInS3);
+        return lastRegitesInS3;
     }
 
-    public static  Integer ClasificationHour(String Daily, String Month){
-        Integer IntDay = 0;
-        String StringDay = "";
-        if(Daily.contains(Month) && Daily.length()>30 && Daily.length() < 35  ){
-            StringDay = Daily.substring(31,Daily.length()-1);
-            StringDay = StringDay.replaceAll("[^0-9]", "");
-            IntDay = Integer.parseInt(StringDay);
-        }
-        return IntDay;
-    }
-//
-    public static  Integer ClasificationDay(String Daily, String Month){
-        Integer IntDay = 0;
-        String StringDay = "";
-        if(Daily.contains(Month) && Daily.length()>22 && Daily.length() < 27  ){
-            StringDay = Daily.substring(20,Daily.length()-1);
-            StringDay = StringDay.replaceAll("[^0-9]", "");
-            IntDay = Integer.parseInt(StringDay);
-            if(StringDay == ""){
-                IntDay = -1;
-            }
-        }
-        return IntDay;
-    }
-
-    public static Integer ClasificationMonth(String Months, Integer Year){
-        Integer IntMonth = 0;
-        String  StringMonth= "";
-        if(Months.length()>10 && Months.length() < 21 && Months.contains(Year.toString())){
-            StringMonth = Months.substring(10, Months.length()-1);
-            StringMonth = StringMonth.replaceAll("[^0-9]", "");
-            IntMonth = Integer.parseInt(StringMonth);
-        }
-        return IntMonth;
-    }
-
-    public  static Integer ClasificationYears(String Data){
-        Integer YearInt = 0;
-        String YearString = "";
-            if(Data.length() < 12){
-               YearString = Data.replaceAll("[^0-9]", "");
-               YearInt = Integer.parseInt(YearString);
-            }
-          return  YearInt;
-        }
-    }
-
-
-
-
-
+}
 
